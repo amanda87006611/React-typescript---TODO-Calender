beb09af508b0c55351b325a8d51ac87ba422cd50
amanda <amanda87006611@gmail.com>
2023-05-11 16:30:13 +0800

    test

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import globalReducer from "./slice/global";
-
-const store = configureStore({
-  reducer: { global: globalReducer },
-});
-
-export default store;
